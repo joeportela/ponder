f2576d4f4c59050a3cc8beb0a3e8fccdde146860
Joe Portela <jportela14@gmail.com>
2024-08-09 16:10:21 -0400

    remove commented trash

--- packages/core/src/config/kafka.ts
+++ packages/core/src/config/kafka.ts
@@ -5,39 +5,3 @@ export type KafkaClusterConfig = {
     password: string;
   };
 };
-
-// export type KafkaConfig = {
-//   cluster: KafkaClusterConfig;
-//   topics: KafkaTopicsConfig;
-// };
-
-// // `Narrow` utility type is inferred from its usage in the NetworksConfig
-// type ExampleMessageSchema = {
-//   foo: string;
-// };
-// const exampleSchema = z.object({
-//   bar: z.string(),
-// });
-
-// const x: KafkaTopicConfig = {
-//   messageSchema: exampleSchema,
-//   topic: "user.events",
-// };
-
-// const y: KafkaTopicsConfig = {
-//   foo: x
-// }
-
-// // Example usage
-// const exampleConfig: KafkaConfig = {
-//   topics: {
-//     userEvents: {
-//       messageSchema: 1,
-//       topic: "user.events",
-//     },
-//     transactionEvents: {
-//       messageSchema: exampleSchema,
-//       topic: "transaction.events",
-//     },
-//   },
-// };
